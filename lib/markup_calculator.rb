171fe3e922cf9ef6070ae22d4d4ebd437cf2650c
Ionut Alexandru Anca <ionut998@gmail.com>
2017-06-03 12:12:15 -0400

    add method for calculating flat markup value

--- lib/markup_calculator.rb
+++ lib/markup_calculator.rb
@@ -1,4 +1,14 @@
 class MarkupCalculator
+  FLAT_MARKUP_PERCENTAGE = 5
+
+  def initialize(base_price)
+    @base_price = base_price
+  end
+
   def repackaging_price
+  end
+
+  def price_including_flat_markup
+    @base_price * ( 100 + FLAT_MARKUP_PERCENTAGE ) / 100
   end
 end
--- spec/markup_calculator_spec.rb
+++ spec/markup_calculator_spec.rb
@@ -1,6 +1,15 @@
-require_relative "spec_helper"
+require_relative 'spec_helper'
 require 'markup_calculator'
 
 describe MarkupCalculator do
+  subject(:markup_calculator){ MarkupCalculator.new(base_price) }
+  let(:base_price){ 100 }
+
   it { is_expected.to respond_to(:repackaging_price) }
+
+  describe '#price_including_flat_markup' do
+    it 'is the base price plus the flat markup value' do
+      expect(subject.price_including_flat_markup).to eq(105)
+    end
+  end
 end
